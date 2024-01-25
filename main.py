92e243c9142cd190d08bba738be7ae3cfb8e7b1f
PIRODEVELEPOR <82955298+hk4crprasad@users.noreply.github.com>
2024-01-25 20:16:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,20 @@ from gemini_pro_bot.bot import start_bot
 from flask import Flask,render_template
 from threading import Thread
 import os
+import streamlit as st
 
+def main():
+    st.title("Simple Streamlit App")
+    
+    # Add some content to your app
+    st.write("Welcome to my Streamlit app!")
+
+    # Add a simple input field
+    user_input = st.text_input("Enter your name:", "Type here...")
+
+    # Display a greeting
+    st.write(f"Hello, {user_input}!")
+    
 app = Flask(__name__)
 @app.route('/')
 def index():
@@ -17,3 +30,4 @@ def keep_alive():
 if __name__ == "__main__":
     keep_alive()
     start_bot()
+    main()
